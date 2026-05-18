6c737ca5807041d6626cd438aa3cee0ccfb15975
Adam Eilers <adam.eilers@gmail.com>
2026-05-18 08:30:25 -0500

    feat: enabling custom sizes for Progress template

--- src/templates/Loading/Loading.tsx
+++ src/templates/Loading/Loading.tsx
@@ -2,14 +2,15 @@ import React, { type FC } from 'react'
 import Column from '../../components/Columns/Column'
 import Columns from '../../components/Columns/Columns'
 import Container from '../../components/Container/Container'
-import Hero from '../../components/Hero/Hero'
+import Hero, { type IHero } from '../../components/Hero/Hero'
 import ProgressBar, { type IProgressBar } from '../../components/ProgressBar/ProgressBar'
 
 export interface ILoading {
   color?: IProgressBar['color']
+  size?: IHero['size']
 }
-const Loading: FC<ILoading> = ({ color = 'primary' }) => (
-  <Hero color='black' size='fullheight'>
+const Loading: FC<ILoading> = ({ color = 'primary', size = 'fullheight' }) => (
+  <Hero color='black' size={size}>
     <Container>
       <Columns vcentered>
         <Column>
